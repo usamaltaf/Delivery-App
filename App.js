a0a3e401f7896e7624de48fe65971433be7464c7
usamaltaf <62393328+usamaltaf@users.noreply.github.com>
2021-01-28 20:38:43 +0500

    Nothing changed

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import WelcomeScreen from "./Screens/WelcomeScreen";
 
-export default function App() {
+export default function App() 
+{
   return <WelcomeScreen />; //WE can only pass One screen Because Return only return one thing
 }
