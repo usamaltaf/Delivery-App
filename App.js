2a6c1b2cb6ab7833d9f375297a1eedd11c0256c6
Mohsintariq1 <mohsintariq1335@gmail.com>
2021-01-28 20:06:26 +0500

    Some changes like app slpah index landingpage welcomescreen

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import React from "react";
-import WelcomeScreen from "./Screens/WelcomeScreen";
+import WelcomeScreen from "./Navigation/Index";
+import { createAppContainer} from "react-navigation";
 
-export default function App() {
-  return <WelcomeScreen />; //WE can only pass One screen Because Return only return one thing
-}
+export default createAppContainer(WelcomeScreen);
